b537605f8a02a392345750e71377ffdbfe7dd4ee
saospina <sergos0617@gmail.com>
2021-03-26 17:33:30 -0500

    installing and implementing Redux

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -4,7 +4,35 @@ import { NotesAppBar } from './NotesAppBar';
 export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
+
             <NotesAppBar />
+
+            <div className="notes__content">
+
+                <input
+                    type="text"
+                    placeholder="An awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    className="notes__textarea"
+                    placeholder="What happended today?"
+                    name=""
+                    id=""
+                    cols="30"
+                    rows="10"
+                ></textarea>
+
+                <div className="notes__image">
+                    <img src="https://wallpaperaccess.com/full/4545965.png" alt="dragon ball z"/>
+
+                </div>
+
+
+
+            </div>
         </div>
     )
 }
