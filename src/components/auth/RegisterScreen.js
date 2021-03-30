21b67039c2a25cd4ca44d1fbbc96beb26db4758d
saospina <sergos0617@gmail.com>
2021-03-29 22:33:47 -0500

    handling register form

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+
+const registerValues = {
+    name: 'sergio',
+    email: 'se@yo.com',
+    password: '123456',
+    password2: '123456'
+}
 
 export const RegisterScreen = () => {
+
+    const [formValues, handleInputChange] = useForm(registerValues)
+    const { name, email, password, password2 } = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log(name, email, password, password2);
+    }
+
     return (
         <div>
             <h3 className="auth__title">Register Screen</h3>
-            <form action="">
+            <form onSubmit={handleRegister}>
                 <input
                     className="auth__input"
                     type="text"
                     placeholder="Name"
                     name="name"
                     autoComplete="off"
+                    value={name}
+                    onChange={handleInputChange}
                 />
                 <input
                     className="auth__input"
@@ -19,6 +38,8 @@ export const RegisterScreen = () => {
                     placeholder="Email"
                     name="email"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
                 <input
                     className="auth__input"
@@ -26,6 +47,8 @@ export const RegisterScreen = () => {
                     placeholder="Password"
                     name="password"
                     autoComplete="off"
+                    value={password}
+                    onChange={handleInputChange}
                 />
                 <input
                     className="auth__input"
@@ -33,6 +56,8 @@ export const RegisterScreen = () => {
                     placeholder="Confirm"
                     name="password2"
                     autoComplete="off"
+                    value={password2}
+                    onChange={handleInputChange}
                 />
                 <button
                     className="btn btn-primary btn-block"
